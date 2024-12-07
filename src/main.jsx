e09506fb178b6ed404dde4ee649bed02e7159aad
Tanjid Karim <you@example.com>
2024-12-07 11:33:50 +0600

    onway to toolkit

--- src/main.jsx
+++ src/main.jsx
@@ -13,7 +13,6 @@ import MyVisaApp from './pages/MyVisaApp';
 import Signin from './components/Signin.jsx';
 import SignUp from './components/SignUp.jsx';
 import MyAddedVisa from './pages/MyAddedVisa.jsx';
-import 'react-tooltip/dist/react-tooltip.css';
 import Errora from './components/Errorar.jsx';
 import PrivateRoute from './components/PrivateRoute.jsx';
 import { ToastContainer } from 'react-toastify';
